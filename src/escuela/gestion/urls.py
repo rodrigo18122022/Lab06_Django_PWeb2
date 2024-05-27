c511cbf2dec4695eb40a741c84c131051e99710e
salasvictor22 <jsalasag@unsa.edu.pe>
2024-05-27 16:21:00 -0500

    Añadiendo las rutas url en el archivo urls.py dentro de la aplicacion gestion y dentro del proyecto escuela

--- src/escuela/gestion/urls.py
+++ src/escuela/gestion/urls.py
@@ -1,1 +1,11 @@
 from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('alumnos/', views.lista_alumnos, name='lista_alumnos'),
+    path('alumnos/crear/', views.crear_alumno, name='crear_alumno'),
+    path('cursos/', views.lista_cursos, name='lista_cursos'),
+    path('cursos/crear/', views.crear_curso, name='crear_curso'),
+    path('notas/', views.lista_notas, name='lista_notas'),
+    path('notas/crear/', views.crear_nota, name='crear_nota'),
+]
