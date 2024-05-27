3a5685fb800e0a1fb844d03c1fa03a57d0c9d50e
salasvictor22 <jsalasag@unsa.edu.pe>
2024-05-26 21:11:00 -0500

    Creacion del archivo forms.py y creando las clases alumno, curso y notas

--- src/escuela/gestion/forms.py
+++ src/escuela/gestion/forms.py
@@ -1,1 +1,17 @@
 from django import forms
+from .models import Alumno, Curso, NotaAlumnosPorCurso
+
+class AlumnoForm(forms.ModelForm):
+    class Meta:
+        model = Alumno
+        fields = ['nombre', 'apellido', 'dni']
+
+class CursoForm(forms.ModelForm):
+    class Meta:
+        model = Curso
+        fields = ['nombre', 'descripcion']
+
+class NotaAlumnosPorCursoForm(forms.ModelForm):
+    class Meta:
+        model = NotaAlumnosPorCurso
+        fields = ['alumno', 'curso', 'nota']
