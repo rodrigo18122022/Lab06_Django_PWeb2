0edb5bba750263338ad66ff84ef0441c4412b0c5
salasvictor22 <jsalasag@unsa.edu.pe>
2024-05-26 20:40:00 -0500

    modificacion del archivo models.py de la aplicacion gestion

--- src/escuela/gestion/models.py
+++ src/escuela/gestion/models.py
@@ -1,2 +1,24 @@
 from django.db import models
 
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    dni = models.CharField(max_length=8, unique=True)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
+class NotaAlumnosPorCurso(models.Model):
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nota = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.alumno} - {self.curso}: {self.nota}"
